c8063c9761bc311c7790d1458cc2617e9b45cc7c
Lucas Mendes Israel <lukasm.israel@gmail.com>
2024-07-01 21:36:05 -0300

    3 - testes de movimento das peças

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -1,4 +1,4 @@
-﻿using xadrez_console.tabuleiro;
+﻿using tabuleiro;
 
 namespace tabuleiro
 {
@@ -39,6 +39,19 @@ namespace tabuleiro
             }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+
+        public Peca retirarPeca(Posicao pos)
+        {
+            if (peca(pos) == null)
+            {
+                return null;
+            }
+
+            Peca aux = peca(pos);
+            aux.posicao = null;
+            pecas[pos.linha, pos.coluna] = null;
+            return aux;
         }
 
         public bool posicaoValida(Posicao pos)
